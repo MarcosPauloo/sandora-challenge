a01677171c1d61f05e0f1ddb8128e009668f29ef
Marcos Paulo <marcos.silva4@arapiraca.ufal.br>
2025-05-22 18:52:38 -0300

    feat: adding redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-
+'use client'
 import { AuthLayout } from "@/components/index";
 
 import { TextLogin, FormLogin } from "@/components/index";
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return (
-    <AuthLayout
-      leftContent={<TextLogin />}
-      rightContent={<FormLogin />}
-    />
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login'); // redireciona para a rota /login
+  }, [router]);
+
+  return null;
 }
